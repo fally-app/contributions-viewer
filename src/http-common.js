7afe76797ea89072869ecfde348c4ce8f060073b
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-26 12:45:43 +0200

    add heroku link

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:1500",
+  baseURL: "https://s-collector.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   },
